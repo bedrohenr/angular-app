076be77d061704d87f0d18e83e7d4d329d6a9bf8
Pedro Ferreira <ferfonhp@gmail.com>
2025-02-14 17:50:15 -0300

    usando @Output ( )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { HomeComponent } from './components/home/home.component';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  logar(event: string){
+    console.log(event);
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { EnviaFormService } from '../../services/envia-form.service';
 
 @Component({
@@ -8,13 +8,16 @@ import { EnviaFormService } from '../../services/envia-form.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  name = "Marcos Daniel Andrea Marcela";
+  name = "Marcos Marcela";
   fruitList = ['Uva', 'Maçã', 'Banana'];
   private enviaForm = inject(EnviaFormService);
 
   @Input("name") props!: string;
 
+  @Output() emitindoValorName = new EventEmitter<string>();
+
   clicked(){
+    this.emitindoValorName.emit(this.name);
     this.enviaForm.enviaInfoParaBackend();
   }
 }
